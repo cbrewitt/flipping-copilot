130743c311fdddf99334c921c52227bbca451baf
cillian <cillian@coflip.net>
2025-04-29 12:25:53 +0100

    add timeframe to separate control panel

--- src/main/java/com/flippingcopilot/ui/CopilotPanel.java
+++ src/main/java/com/flippingcopilot/ui/CopilotPanel.java
@@ -1,6 +1,5 @@
 package com.flippingcopilot.ui;
 
-
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.swing.*;
@@ -11,16 +10,22 @@ public class CopilotPanel extends JPanel {
 
     public final SuggestionPanel suggestionPanel;
     public final StatsPanelV2 statsPanel;
+    public final ControlPanel controlPanel;
 
     @Inject
     public CopilotPanel(SuggestionPanel suggestionPanel,
-                        StatsPanelV2 statsPanel) {
+                        StatsPanelV2 statsPanel,
+                        ControlPanel controlPanel) {
         this.statsPanel = statsPanel;
         this.suggestionPanel = suggestionPanel;
+        this.controlPanel = controlPanel;
+        
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         suggestionPanel.setMaximumSize(new Dimension(Integer.MAX_VALUE, suggestionPanel.getPreferredSize().height));
         suggestionPanel.setMinimumSize(new Dimension(Integer.MIN_VALUE, suggestionPanel.getPreferredSize().height));
         add(suggestionPanel);
+        add(Box.createRigidArea(new Dimension(0, 5)));
+        add(controlPanel);
         add(Box.createRigidArea(new Dimension(0, 5)));
         add(Box.createVerticalGlue());
         add(statsPanel);
@@ -33,5 +38,6 @@ public class CopilotPanel extends JPanel {
             return;
         }
         suggestionPanel.refresh();
+        controlPanel.refresh();
     }
 }
--- src/main/java/com/flippingcopilot/ui/PreferencesPanel.java
+++ src/main/java/com/flippingcopilot/ui/PreferencesPanel.java
@@ -18,11 +18,6 @@ public class PreferencesPanel extends JPanel {
     private final JPanel f2pOnlyButton;
     private final PreferencesToggleButton f2pOnlyModeToggleButton;
     private final BlacklistDropdownPanel blacklistDropdownPanel;
-    private final JPanel timeframePanel;
-    private final JToggleButton btn5m;
-    private final JToggleButton btn30m;
-    private final JToggleButton btn2h;
-    private final JToggleButton btn8h;
 
     @Inject
     public PreferencesPanel(
@@ -46,38 +41,6 @@ public class PreferencesPanel extends JPanel {
         preferencesTitle.setHorizontalAlignment(SwingConstants.CENTER);
         add(preferencesTitle);
         add(Box.createRigidArea(new Dimension(0, 6)));
-
-        // Add timeframe buttons
-        timeframePanel = new JPanel();
-        timeframePanel.setLayout(new BoxLayout(timeframePanel, BoxLayout.Y_AXIS));
-        timeframePanel.setOpaque(false);
-        JLabel timeframeLabel = new JLabel("How often do you check offers?");
-        timeframeLabel.setHorizontalAlignment(SwingConstants.LEFT);
-        timeframeLabel.setMaximumSize(timeframeLabel.getPreferredSize());
-        JPanel labelPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
-        labelPanel.setOpaque(false);
-        labelPanel.add(timeframeLabel);
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new GridLayout(1, 4, 0, 0));
-        buttonPanel.setOpaque(false);
-        ButtonGroup timeframeButtonGroup = new ButtonGroup();
-        btn5m = createTimeframeButton("5m", 5, suggestionManager);
-        btn30m = createTimeframeButton("30m", 30, suggestionManager);
-        btn2h = createTimeframeButton("2h", 120, suggestionManager);
-        btn8h = createTimeframeButton("8h", 480, suggestionManager);
-        timeframeButtonGroup.add(btn5m);
-        timeframeButtonGroup.add(btn30m);
-        timeframeButtonGroup.add(btn2h);
-        timeframeButtonGroup.add(btn8h);
-        buttonPanel.add(btn5m);
-        buttonPanel.add(btn30m);
-        buttonPanel.add(btn2h);
-        buttonPanel.add(btn8h);
-        timeframePanel.add(labelPanel);
-        timeframePanel.add(Box.createRigidArea(new Dimension(0, 3)));
-        timeframePanel.add(buttonPanel);
-        add(timeframePanel);
-        add(Box.createRigidArea(new Dimension(0, 3)));
 
         sellOnlyModeToggleButton = new PreferencesToggleButton("Disable sell-only mode", "Enable sell-only mode");
         sellOnlyButton = new JPanel();
@@ -110,35 +73,6 @@ public class PreferencesPanel extends JPanel {
 
         add(Box.createRigidArea(new Dimension(0, 3)));
         add(this.blacklistDropdownPanel);
-    }    
-
-    private JToggleButton createTimeframeButton(String label, int value, SuggestionManager suggestionManager) {
-        JToggleButton button = new JToggleButton();
-        button.addActionListener(e -> {
-            preferencesManager.setTimeframe(value);
-            suggestionManager.setSuggestionNeeded(true);
-        });
-        button.setMargin(new Insets(2, 4, 2, 4));
-        button.setFocusPainted(false);
-        button.setOpaque(true);
-        button.setBackground(ColorScheme.DARKER_GRAY_COLOR);
-        button.setForeground(ColorScheme.TEXT_COLOR);
-
-        // Set initial text as white
-        button.setText("<html><font color='rgb(198, 198, 198)'>" + label + "</font></html>");
-
-        // Change color and text when selected/unselected
-        button.addChangeListener(e -> {
-            if (button.isSelected()) {
-                button.setBackground(ColorScheme.BRAND_ORANGE);
-                button.setText("<html><font color='black'>" + label + "</font></html>");
-            } else {
-                button.setBackground(ColorScheme.DARKER_GRAY_COLOR);
-                button.setText("<html><font color='rgb(198, 198, 198)'>" + label + "</font></html>");
-            }
-        });
-
-        return button;
     }
 
     public void refresh() {
@@ -153,11 +87,5 @@ public class PreferencesPanel extends JPanel {
         f2pOnlyModeToggleButton.setSelected(preferencesManager.getPreferences().isF2pOnlyMode());
         f2pOnlyButton.setVisible(true);
         blacklistDropdownPanel.setVisible(true);
-        timeframePanel.setVisible(true);
-        int tf = preferencesManager.getTimeframe();
-        btn5m.setSelected(tf == 5);
-        btn30m.setSelected(tf == 30);
-        btn2h.setSelected(tf == 120);
-        btn8h.setSelected(tf == 480);
     }
 }
