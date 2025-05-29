1593064ecb155dec5b68acda6b0025a48a6ffd60
cillian <cillian@coflip.net>
2025-05-29 11:13:05 +0100

    increase tax rate to 2%

--- src/main/java/com/flippingcopilot/util/GeTax.java
+++ src/main/java/com/flippingcopilot/util/GeTax.java
@@ -6,7 +6,7 @@ import java.util.HashSet;
 public class GeTax {
     private final static int MAX_PRICE_FOR_GE_TAX = 500000000;
     private final static int GE_TAX_CAP = 5000000;
-    private final static double GE_TAX = 0.01;
+    private final static double GE_TAX = 0.02;
     private final static HashSet<Integer> GE_TAX_EXEMPT_ITEMS = new HashSet<>(
             Arrays.asList(13190, 1755, 5325, 1785, 2347, 1733, 233, 5341, 8794, 5329, 5343, 1735, 952, 5331));
 
