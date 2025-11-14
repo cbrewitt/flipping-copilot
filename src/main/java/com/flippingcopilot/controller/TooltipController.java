414e45976c6ecc226f9e85a28bad378b833eee5a
Joss Peters <jptrs93@gmail.com>
2025-11-15 07:18:15 +1100

    Add text revalidate on profit tooltip

--- src/main/java/com/flippingcopilot/controller/TooltipController.java
+++ src/main/java/com/flippingcopilot/controller/TooltipController.java
@@ -78,6 +78,7 @@ public class TooltipController {
                 tooltip.revalidate();
                 border.revalidate();
                 background.revalidate();
+                text.revalidate();
             }
         }
     }
